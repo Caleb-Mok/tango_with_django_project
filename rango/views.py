2321ddef4523e8595522a487dfdb33ad00c9b8b2
Caleb Mok <2689719m@student.gla.ac.uk>
2023-02-09 20:51:53 +0000

    Chp 4

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+    context_dict = {'boldmessage': 'This tutorial has been put together by Caleb.'}
+    return render(request, 'rango/about.html', context=context_dict)
+
 
