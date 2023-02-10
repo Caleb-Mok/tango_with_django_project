5edae207924de189da1f7083a44a6c23f9939054
Caleb Mok <2689719m@student.gla.ac.uk>
2023-02-10 15:00:06 +0000

    Chp 10

--- rango/views.py
+++ rango/views.py
@@ -11,6 +11,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 # Create your views here.
@@ -31,12 +32,17 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-
-    context_dict = {'boldmessage': 'This tutorial has been put together by Caleb.'}
+    
+    context_dict ={}
+    context_dict['visits']=request.session['visits']
+    context_dict['boldmessage']= 'This tutorial has been put together by Caleb.'
+    
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -151,11 +157,13 @@ def register(request):
             # Now we hash the password with the set_password method.
             # Once hashed, we can update the user object.
             user.set_password(user.password)
-            user.save()
+            user.save()
+
 
             # Now sort out the UserProfile instance.
             # Since we need to set the user attribute ourselves,
-            # we set commit=False. This delays saving the model            
+            # we set commit=False. This delays saving the model
+            
             # until we're ready to avoid integrity problems.
             profile = profile_form.save(commit=False)
             profile.user = user
@@ -240,4 +248,33 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    
+    last_visit_cookie = get_server_side_cookie(request,
+                                                'last_visit',
+                                                str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -104,7 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGIN_URL = 'rango:login'
+LOGIN_URL = 'rango:login'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
